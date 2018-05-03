3ca8385a85737433f0b320ba51033e92f945b437
zhiwutian <brettalbright@outlook.com>
2018-05-02 18:29:40 -0700

    completed to do list demo

---
+++ src/components/add-items.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+
+class AddItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            details: ""
+        }
+
+    }
+
+    handleAddItem(event) {
+        event.preventDefault();
+        console.log("item:", this.state);
+
+        this.props.add(this.state);
+    }
+
+
+    render() {
+
+        const {title, details} = this.state;
+        return (
+            <form onSubmit={this.handleAddItem.bind(this)}>
+                <div className="row">
+                    <div className="col s12 m8 offset-m2">
+                        <label>Title</label>
+                        <input value={title} type="text" onChange={ (event) => this.setState({title: event.target.value})}/>
+                    </div>
+
+                    <div className="col s12 m8 offset-m2">
+                        <label>Details</label>
+                        <input value={details} type="text" onChange={ event => this.setState({details: event.target.value})}/>
+                    </div>
+                </div>
+                <div className="row">
+                <div className="center-align col s12 m8 offset-m2">
+                    <button className="btn purple darken-4">Add Item</button>
+                </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+
+export default AddItem;
